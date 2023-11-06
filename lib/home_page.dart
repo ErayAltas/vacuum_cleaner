eec22e0fde2bb0383ab4be5d68f8c47fbcb4deb8
ErayAltas <eray.altas@stu.fsm.edu.tr>
2023-11-06 04:44:16 -0800

    updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,8 +15,11 @@ class _HomePageState extends State<HomePage> {
   Room roomA = Room();
   Room roomB = Room();
   VacuumCleaner vacuumCleaner = VacuumCleaner();
-  late Timer timer;
-  int remainingTime = 120;
+  late Timer roomATimer;
+  late Timer roombTimer;
+  late Timer systemTimer;
+  late Timer cleaningTimer;
+  int totalSystemTime = 60;
   List<String> systemLogs = [];
   TextEditingController systemLogsController = TextEditingController();
   ScrollController textFieldScrollController = ScrollController();
@@ -26,45 +29,109 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     roomA.isDirty = true;
     roomB.isDirty = true;
-    startTimer();
-
     systemLogsController.addListener(() {
-      textFieldScrollController.jumpTo(textFieldScrollController.position.maxScrollExtent);
+      setState(() {
+        textFieldScrollController.jumpTo(textFieldScrollController.position.maxScrollExtent);
+      });
     });
   }
 
   @override
   void dispose() {
-    timer.cancel();
+    cancelTimers();
     super.dispose();
+  }
+
+  void cancelTimers() {
+    systemTimer.cancel();
+    roomATimer.cancel();
+    roombTimer.cancel();
+    cleaningTimer.cancel();
+  }
+
+  void startSystem() {
+    systemLogs.add("system is starting");
+    systemLogsController.text = systemLogs.join("\n");
+    startCleaningTimer();
+    systemTimer = Timer.periodic(
+      const Duration(seconds: 1),
+      (Timer timer) {
+        setState(() {
+          totalSystemTime--;
+        });
+        if (totalSystemTime == 0) {
+          cancelTimers();
+          systemLogs.add("system is shutting down");
+          systemLogsController.text = systemLogs.join("\n");
+        }
+      },
+    );
+  }
+
+  void startCleaningTimer() {
+    cleaningTimer = Timer.periodic(
+      const Duration(seconds: 10),
+      (Timer timer) {
+        decideWhichRoom();
+      },
+    );
+  }
+
+  void decideWhichRoom() {
+    systemLogs.add("Vacuum cleaner controlling room A and room B");
+    systemLogsController.text = systemLogs.join("\n");
+    if (roomA.isDirty) {
+      cleanRoomA();
+    } else if (roomB.isDirty) {
+      cleanRoomB();
+    } else {
+      systemLogs.add("Both rooms are clean");
+      systemLogsController.text = systemLogs.join("\n");
+    }
   }
 
   Future<void> cleanRoomA() async {
     systemLogs.add("Vacuum cleaner is cleaning room A");
+    systemLogsController.text = systemLogs.join("\n");
     setState(() {});
     await Future.delayed(const Duration(seconds: 5));
     systemLogs.add("Vacuum cleaner finished cleaning room A");
     systemLogsController.text = systemLogs.join("\n");
     roomA.isDirty = false;
     setState(() {});
+    startRoomATimer();
   }
 
   Future<void> cleanRoomB() async {
     systemLogs.add("Vacuum cleaner is cleaning room B");
+    systemLogsController.text = systemLogs.join("\n");
     setState(() {});
     await Future.delayed(const Duration(seconds: 5));
     systemLogs.add("Vacuum cleaner finished cleaning room B");
     systemLogsController.text = systemLogs.join("\n");
     roomB.isDirty = false;
     setState(() {});
+    startRoomBTimer();
   }
 
-  void startTimer() {
-    timer = Timer.periodic(
-      const Duration(seconds: 5),
+  void startRoomATimer() {
+    roomATimer = Timer.periodic(
+      const Duration(seconds: 20),
       (Timer timer) {
-        cleanRoomA();
-        cleanRoomB();
+        setState(() {
+          roomA.isDirty = true;
+        });
+      },
+    );
+  }
+
+  void startRoomBTimer() {
+    roombTimer = Timer.periodic(
+      const Duration(seconds: 20),
+      (Timer timer) {
+        setState(() {
+          roomB.isDirty = true;
+        });
       },
     );
   }
@@ -110,35 +177,12 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-              // Row(
-              //   children: [
-              //     ElevatedButton(
-              //       onPressed: () {
-              //         startTimer();
-              //       },
-              //       child: const Text("-"),
-              //     ),
-              //     const SizedBox(
-              //       width: 50,
-              //       child: TextField(
-              //         decoration: InputDecoration(
-              //           border: OutlineInputBorder(),
-              //           contentPadding: EdgeInsets.zero,
-              //         ),
-              //       ),
-              //     ),
-              //     ElevatedButton(
-              //       onPressed: () {
-              //         startTimer();
-              //       },
-              //       child: const Text("+"),
-              //     ),
-              //   ],
-              // ),
+              const SizedBox(height: 20),
+              Text("System time : $totalSystemTime"),
               const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  startTimer();
+                  startSystem();
                 },
                 child: const Text("Start"),
               ),
